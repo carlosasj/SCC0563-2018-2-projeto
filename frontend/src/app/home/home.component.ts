2df075803b31dc443a7110ea4bd33b1ed15c1bf5
Carlos Schneider <carlosaschjr@gmail.com>
2018-09-09 15:03:54 -0300

    Começa a criar as telas

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnDestroy {
 
-  constructor() { }
+  private entries = [
+    'Programmer',
+    'Designer',
+    'iOS Developer',
+    'Nurse',
+    'Profesor',
+    'Cashier',
+    'Musician'
+  ];
+  public placeholder = this.entries[0];
+  private counter = 0;
+  private timer = null;
 
-  ngOnInit() {
+  constructor() {
+    this.timer = setInterval(() => {
+      this.counter++;
+      this.counter = this.counter % this.entries.length;
+      this.placeholder = this.entries[this.counter];
+    }, 2500)
+  }
+
+  ngOnDestroy() {
+    if (this.timer) {clearInterval(this.timer); }
   }
 
 }
